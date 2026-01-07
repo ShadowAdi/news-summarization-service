b608012b4a4153cd73e1351a0c84039d124105af
ShadowAdi <shadowshukla76@gmail.com>
2026-01-07 16:07:27 +0530

    quick fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ArticlesController } from './articles/articles.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ArticlesController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
